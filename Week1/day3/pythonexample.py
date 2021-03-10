8a5aebc82f4956194d5b88b1e59ff4e645b6a98e
Randi Reyna <randireyna@live.com>
2021-03-10 11:01:17 -0600

    made changes

--- Week1/day3/pythonexample.py
+++ Week1/day3/pythonexample.py
@@ -2,6 +2,9 @@
 # nameofVariable = assignedValieToVariable
 # String === "", "word", "1", "functionName"
 greeting = "Welcome to Digital Crafts Boot Camp"
+classSize = 23
+somethingToCallIt = ['students', 23, True]
+somethingToCallIt = {"helloPythonWorld": 23, "we are students": 24.5}
 print(greeting)
 # List, sequence of values followed by [value1, value2, etc]
 #[1, 2, 3]
@@ -11,3 +14,7 @@ print(greeting)
 #{"stringName": "StringValue"}
 # {"FavoriteColor": "Red"}
 # Name, assignment operator (=), value that I am assigning
+print("I am programming")
+print(classSize)
+print(somethingToCallIt)
+print(somethingToCallIt)
