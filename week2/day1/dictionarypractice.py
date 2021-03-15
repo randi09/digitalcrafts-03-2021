d93e54646c31438374307ce3624d4e4ca4374e88
Randi Reyna <randireyna@randis-air.attlocal.net>
2021-03-15 16:37:28 -0500

    added new files to mac

---
+++ week2/day1/dictionarypractice.py
@@ -0,0 +1,63 @@
+# add your info
+#
+
+# add a platform ,ps5, pc, xbox 1, switch, add your skill level ("noob", "not", "noob")
+gamer = {
+    "platform": ["Sega Genesis"],
+    "gamingHours": [{"weekday": "9-5"}, {"weekends": "anytime"}],
+    "skill": ["expert"]
+}
+
+
+# print on the terminal, what time you would game?
+
+
+
+
+digitalCraftsStudent = {
+    "name": "",
+    "city": "",
+    "computer": [],
+}
+gamer = {
+    "platform": "",
+    "gamingHours": [{"weekday": "9-5"}, {"weekends": "anytime"}],
+    "skill": ''
+}
+print(gamer)
+
+# 1. print out a list of engine choices (the whole list)
+# 2. print out the horsepower values only (all of them)
+# 3. check to see if the key "trim" exist on the car dictionary
+# 4. add a new key to the car dictionary called "colors" which will be a list of all possible colors (use 4 color strings)
+# 5. find an image and put the url into https://bitly.com/, and paste shortened url into that "image" value
+
+
+car = {
+    "model": ["Chevy"],
+    "image": "https://bit.ly/3tlZApN",
+    "year": ["2017"],
+    "engineChoices": [
+        {
+            "v4": {
+                "horsepower": 230
+            }
+        },
+        {
+            "v6": {
+                "horsepower": 300
+            }
+        },
+        {
+            "v8": {
+                "horsepower": 400
+            }
+        },
+        {
+            "v12": {
+                "horsepower": 900
+            }
+        }
+    ],
+}
+
